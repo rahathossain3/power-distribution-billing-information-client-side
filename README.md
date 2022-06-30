5b587f051c179c01d2d224882beb623382914297
rahathossain3 <rahathossain210@gmail.com>
2022-06-30 15:51:43 +0600

    project start

---
+++ README.md
@@ -0,0 +1,8 @@
+# Project Name: power-distribution-billing-information
+
+## Uses: 
+
+* React router
+* Tailwind css
+* Daisy Ui
+* 
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className=' text-2xl'>ok</h2>
+      <button class="btn btn-accent btn-outline">Three</button>
+    </div>
+  );
+}
+
+export default App;
